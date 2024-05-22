4a21578414b7f77b5c873ad006226c650d629f3c
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-22 16:14:54 -0500

    Update checks.py

--- checks.py
+++ checks.py
@@ -1,24 +1,28 @@
-import spacy
-spacy.cli.download("en_core_web_sm")
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
+import json
 
-def are_sentences_similar_spacy(sentence1, sentence2, threshold=0.8):
-    """
-    Compare two sentences and determine if they are similar using spaCy.
-    
-    :param sentence1: First sentence
-    :param sentence2: Second sentence
-    :param threshold: Similarity threshold (0 to 1), default is 0.8
-    :return: True if sentences are similar, False otherwise
-    """
-    doc1 = nlp(sentence1)
-    doc2 = nlp(sentence2)
-    similarity = doc1.similarity(doc2)
-    return similarity >= threshold
+def ground_truth(video, qs):
+    gt = []
+    for v, info in video.items():
+        name = v.split("_")[0]+'_x'
+        q = qs[name]
+        if len(q['questions'])!=len(info['steps']):
+            gt.append((v, len(q['questions']), len(info['steps'])))
 
-# Example usage
-sentence1 = "Open-Open a can of tuna"
-sentence2 = "Is the person opening a can of tuna?"
+    return gt
 
-print(are_sentences_similar_spacy(sentence1, sentence2))
+def main():
+    json_file = './step_annotations.json'
+    qs_file = './questions.json'
+    with open(json_file, 'r') as f:
+        cont = json.load(f)
+
+    with open(qs_file, 'r') as file:
+        qs = json.load(file)
+
+    lists = ground_truth(cont, qs)
+
+    for i in lists:
+        print(i)
+
+if __name__=='__main__':
+    main()
