253a1079f00a904066f51a4442c28f530fadf621
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-24 11:29:52 -0500

    Update data_metrics.py

--- data_metrics.py
+++ data_metrics.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 accuracy_file = './accuracy.csv'
 
-df = pd.read_csv(accuracy_file)
-print(df.columns)
-df['video-llava - accuracy'] = pd.to_numeric(df['video-llava accuracy'], errors='coerce')
-overall_accuracy = df['video-llava accuracy'].mean()
+df = pd.read(accuracy_file)
+
+df['video-llava - accuracy'] = pd.to_numeric(df['video-llava - accuracy'], errors='coerce')
+
+# Calculate the mean of the accuracy values, ignoring NaN values
+overall_accuracy = df['video-llava - accuracy'].mean()
 
 print(f'Overall Accuracy: {overall_accuracy:.2f}')
