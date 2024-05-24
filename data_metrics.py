627e72d17b7bfb6dcd56b8341f0da89f7a01c369
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-05-24 11:28:13 -0500

    Update data_metrics.py

--- data_metrics.py
+++ data_metrics.py
@@ -3,6 +3,7 @@ import pandas as pd
 accuracy_file = './accuracy.csv'
 
 df = pd.read_csv(accuracy_file)
+print(df.columns)
 df['accuracy'] = pd.to_numeric(df['video-llava accuracy'], errors='coerce')
 overall_accuracy = df['video-llava accuracy'].mean()
 
