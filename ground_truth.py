55a2717fb4d97a2fc47969d2e3d5ae788c472c8c
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-06-04 14:15:57 -0500

    Update ground_truth.py

--- ground_truth.py
+++ ground_truth.py
@@ -41,20 +41,14 @@ def ground_truth(name, video, normal_annot, questions):
     video_steps_desc = [step['description'] for step in steps]
     common_steps = list(set(n_steps_desc).intersection(video_steps_desc))
     q = len(questions)
-    flattened_questions = []
-    for i, j in enumerate(questions):
-        flattened_questions.append(j['q'])
-        flattened_questions.extend(j['followup'])
     
-    print("total length: ",(len(flattened_questions)==len(common_steps)))
-    
-    gt = [0] * len(flattened_questions)
+    gt = [0] * q
 
     for step in steps:
         if step['description'] in common_steps:
             index = common_steps.index(step['description'])
-            question = flattened_questions[index]
-            if step['has_errors'] and "Order Error" in step['errors']:
+            question = questions[index]
+            if step['has_errors'] and "Preparation Error" in step['errors']:
                     gt[index] = 1
 
     current_index = q
@@ -115,12 +109,12 @@ def error_gt(video_dir, q_file, normal_annot, steps, error_type):
 
 def main():
     video_dir = '/data/rohith/captain_cook/videos/gopro/resolution_360p/'
-    m_file = './error_prompts/order_error.json'
+    m_file = './error_prompts/preparation_error.json'
     normal_annot_file = './normal_videos.json'
     steps = './step_annotations.json'
 
-    print("Order error type: ")
-    error_gt(video_dir, m_file, normal_annot_file, steps, 'Order Error')
+    print("Preparation error type: ")
+    error_gt(video_dir, m_file, normal_annot_file, steps, 'Preparation Error')
 
 if __name__ == "__main__":
     main()
