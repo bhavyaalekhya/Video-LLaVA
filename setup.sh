aebaa3563fbcfbbc675ccf3aa7e4b095d87b3506
Bhavya Alekhya <bhavyaalekhya98@gmail.com>
2024-06-04 21:28:45 -0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+conda create -n videollava python=3.10 -y
+conda activate videollava
+pip install --upgrade pip  # enable PEP 660 support
+pip install -e .
+pip install -e ".[train]"
+pip install flash-attn --no-build-isolation
+pip install decord opencv-python git+https://github.com/facebookresearch/pytorchvideo.git@28fe037d212663c6a24f373b94cc5d478c8c1a1d
